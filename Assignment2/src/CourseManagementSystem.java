5a9cf8cafb27642966e330d903b19b2d41cd542b
Josh Junnola <joshjunnola@gmail.com>
2026-03-18 20:15:38 -0400

    removed auto implemented string, added arraylist

--- Assignment2/src/CourseManagementSystem.java
+++ Assignment2/src/CourseManagementSystem.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
+
 public class CourseManagementSystem {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    private ArrayList<Course> coursesTaught = new ArrayList<>();
+
 }
