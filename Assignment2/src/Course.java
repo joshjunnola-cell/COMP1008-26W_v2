55c94d5785bd11215dfd4408de96b73cf2622364
KasibM <129322185+KasibM@users.noreply.github.com>
2026-03-18 20:40:11 -0400

    Course class base and derived class structure

--- Assignment2/src/Course.java
+++ Assignment2/src/Course.java
@@ -1,3 +1,27 @@
-public class Course {
+abstract class Course {
     
+    private String code;
+    private String name;
+    
+    // Constructor 
+    public Course(String code, String name){
+        // Initialize fields
+        this.code = code;
+        this.name = name;
+
+    }
+
+    
+
+}
+
+class InPersonCourse extends Course{
+
+}
+class OnlineCourse extends Course{
+
+}
+
+class HybdridCourse extends Course{
+
 }
