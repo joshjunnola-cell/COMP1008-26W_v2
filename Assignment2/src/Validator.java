ae9867f25c088d5bf9a92d3266fb3e0ccdd165e5
KasibM <129322185+KasibM@users.noreply.github.com>
2026-03-19 15:18:40 -0400

    Document format alteration categorizing methods

--- Assignment2/src/Validator.java
+++ Assignment2/src/Validator.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 public class Validator {
 
-    public static boolean validateStudentId(String input){
+    // ========== Regex Validation Tools
+
+    //Validate Student ID Format
+    private static boolean validateStudentId(String input){
         //Set Student ID format
         String studentIdRegex = "^[sS]-[0-9]{4}$";
         //Return wether the Student ID passed is in the correct format
         return input.matches(studentIdRegex);
     }
 
-    public static boolean validateEmail(String input){
+    //Validate Email Format
+    private static boolean validateEmail(String input){
         //Set Email format
         String emailRegex = "^[A-za-z0-9+_.-]+@[A-za-z0-9+_.-]+$";
         //Return wether the email passed in in the correct format
@@ -18,6 +22,9 @@ public class Validator {
     }
 
 
+    // ========== Input Tools
+    
+    //Request Student ID
     public static String inputValidStudentId(){
         //Open instance of Scanner object from java.util.Scanner
         Scanner scanner = new Scanner(System.in);
@@ -42,6 +49,7 @@ public class Validator {
         return input.toUpperCase();
     }
 
+    //Request Email
     public static String inputValidEmail(){
         //Open instance of Scanner object from java.util.Scanner
         Scanner scanner = new Scanner(System.in);
