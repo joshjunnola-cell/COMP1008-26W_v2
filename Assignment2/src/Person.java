90480554fa55301a406875a25f31f52585fc3b4a
Josh Junnola <joshjunnola@gmail.com>
2026-03-18 20:16:18 -0400

    added abstract classes and comments

--- Assignment2/src/Person.java
+++ Assignment2/src/Person.java
@@ -1,12 +1,22 @@
-abstract class Person {
-    
+public abstract class Person {
+    //Abstract class contains common fields (name, ID, email)
+    private String name;
+    private String id;
+    private String email;
+
+    public Person(String name, String id, String email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
 
 }
-
+    //Extend Person in child classes
 class Student extends Person {
 
 }
 
+    //Extend Person in child classes
 class Instructor extends Person {
 
 }
