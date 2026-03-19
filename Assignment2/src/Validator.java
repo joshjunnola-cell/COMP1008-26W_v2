899af86b7a7b820e7bc173f8f0df15a3930c1189
KasibM <129322185+KasibM@users.noreply.github.com>
2026-03-19 13:26:04 -0400

    validateStudentId method complete

--- Assignment2/src/Validator.java
+++ Assignment2/src/Validator.java
@@ -3,8 +3,11 @@
 
 public class Validator {
 
-    public static void validateStudentId(String input){
-
+    public static boolean validateStudentId(String input){
+        //Set Student ID format
+        String studentIdRegex = "^[sS]-[0-9]{4}$";
+        //return wether the Student ID passed is in the correct format
+        return input.matches(studentIdRegex);
     }
 
     public static void validateEmail(String input){
