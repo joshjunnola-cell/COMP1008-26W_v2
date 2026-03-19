8fbe34610fd70790335cc2e20dbc9eedd369b489
KasibM <129322185+KasibM@users.noreply.github.com>
2026-03-18 20:16:02 -0400

    Validator class and initial validation methods added

--- Assignment2/src/Validator.java
+++ Assignment2/src/Validator.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class Validator {
+
+    public Validator(){
+    }
+
+    public String validateStudentId(Scanner scanner){
+
+        return "";
+    }
+
+    public String validateEmail(Scanner scanner){
+        
+        return "";
+    }
     
 }
+
