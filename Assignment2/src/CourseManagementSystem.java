37f6bafbc8d6ed07ff9c9c254f01cc483a2cd247
KasibM <129322185+KasibM@users.noreply.github.com>
2026-03-19 23:43:04 -0400

    personMenu and courseMenu methods added

--- Assignment2/src/CourseManagementSystem.java
+++ Assignment2/src/CourseManagementSystem.java
@@ -8,33 +8,80 @@ public class CourseManagementSystem {
     
     
     
-    
-    public static void personMenu(){
+    public static void displayInfo(Scanner scanner, String object){
+
+    }
+    public static void add(Scanner scanner, String object){
+
+    }
+    public static void remove(Scanner scanner, String object){
+
+    }
+
+    public static void searchByName(Scanner scanner){
+
+    }
+    public static void searchByID(Scanner scanner){
         
     }
-    public static void courseMenu(){
-
+    public static void personMenu(Scanner scanner){
+        int localChoice = 0;
+        boolean localMenuActive = true;
+        while(localMenuActive){
+            //Menu configuration using choice variable and menuChoice method
+                localChoice = Validator.menuChoice(scanner, "Person Menu", "Show All","Add Person", "Remove Person", "Search by Name", "Search by ID", "Exit");
+                //Main menu switch case choices
+                switch (localChoice) {
+                    case 1:
+                        displayInfo(scanner, "Person");
+                        break;
+                    case 2:
+                        add(scanner, "Person");
+                        break;
+                    case 3:
+                        remove(scanner, "Person");
+                        break;
+                    case 4:
+                        searchByName(scanner);
+                        break;
+                    case 5:
+                        searchByID(scanner);
+                        break;
+                    case 6:
+                        localMenuActive = false;
+                        break;
+                }
+        }
+        
     }
-    public static void populate(){
+    public static void courseMenu(Scanner scanner){
+        int localChoice = 0;
+        boolean localMenuActive = true;
+        while(localMenuActive){
+            //Menu configuration using choice variable and menuChoice method
+                localChoice = Validator.menuChoice(scanner, "Course Menu", "Show All","Add Course", "Remove Course", "Exit");
+                //Main menu switch case choices
+                switch (localChoice) {
+                    case 1:
+                        displayInfo(scanner, "Course");
+                        break;
+                    case 2:
+                        add(scanner, "Course");
+                        break;
+                    case 3:
+                        remove(scanner, "Course");
+                        break;
+                    case 4:
+                        localMenuActive = false;
+                        break;
+                }
+        }
+    }
+    public static void populate(Scanner scanner){
 
     }
     
-    public static void displayInfo(String object){
-
-    }
-    public static void add(String object){
-
-    }
-    public static void remove(String object){
-
-    }
-
-    public static void searchByName(String object){
-
-    }
-    public static void searchByID(String object){
-        
-    }
+    
 
     
 
@@ -58,16 +105,16 @@ public class CourseManagementSystem {
             //Main menu switch case choices
             switch (choice) {
                 case 1:
-
+                    personMenu(scanner);
                     break;
                 case 2:
-
+                    courseMenu(scanner);
                     break;
                 case 3:
-
+                    populate(scanner);
                     break;
                 case 4:
-
+                    courseManagementSystemActive = false;
                     break;
             }
         }
