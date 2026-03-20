188e89e028f418afb5d625d6509afda80b6417d5
Josh Junnola <joshjunnola@gmail.com>
2026-03-19 21:07:42 -0400

    added getters and setters to person class. added program string to student class and finished building it out. started working on instructor

--- Assignment2/src/Person.java
+++ Assignment2/src/Person.java
@@ -11,19 +11,75 @@ public abstract class Person {
         this.email = email;
     }
 
-    //Getters
-    public abstract String getName();
-    public abstract String getId();
-    public abstract String getEmail();
+    public abstract int getPersonType();
 
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = (!(name == null || name.isBlank())) ? name : this.name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //Abstract for polymorphism
+    public abstract String getDetails();
     
 }
     //Extend Person in child classes
 class Student extends Person {
     
+    private String program;
+    //Creates a new student object
+    public Student(String name, String id, String email, String program) {
+        super(name, id, email);
+        this.program = program;
+    }
+
+    @Override
+    public int getPersonType(){
+        return 1;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Student: " + getName() +
+                " | ID: " + getId() +
+                " | Email: " + getEmail() + 
+                " | Program: " + program;
+    }
 }
 
     //Extend Person in child classes
 class Instructor extends Person {
+
+    private String department;
+    //Creates a new instructor object
+    public Instructor(String name, String id, String email, String department) {
+        super(name, id, email);
+        this.department = department;
+    }
+
+    @Override
+    public int getPersonType(){
+        return 2;
+    }
 
 }
